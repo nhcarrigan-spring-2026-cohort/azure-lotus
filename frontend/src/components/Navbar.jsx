a6714eca66cbe748243f040e8e9dd716c2f5f7ed
ArisenLight <cjdonnelly2001@gmail.com>
2026-02-03 20:30:08 +1000

    set up routing, navbar, pages

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <>
+    <header className="navbar">
+        <div className="navbar-left">
+        <div className="logo">
+          <div className="logo-icon">👤</div>
+          <span className="logo-text">Senior Checkin</span>
+        </div>
+        </div>
+        
+      <nav className="navbar-right">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/explained">How it works</Link>
+        <Link to="/signup">Signup</Link>
+      </nav>
+    </header>
+
+    </>
+  );
+}
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import "./Home.css"
+
+export default function Home() {
+  return (
+    <>
+    
+    <h1>Home</h1>
+    
+    </>
+  );
+}
