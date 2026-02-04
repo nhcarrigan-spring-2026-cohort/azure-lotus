c59a539c9acd23b8af52b255416f95ad3df4c1f6
Cheryl Murphy <6191116+cherylli@users.noreply.github.com>
2026-02-05 00:58:16 +1100

    setup react query and auth context providers

--- frontend/src/pages/signup.jsx
+++ frontend/src/pages/signup.jsx
@@ -1,7 +1,14 @@
 export default function signup() {
+
+    const handleRegister = async (e) => {
+        e.preventDefault()
+        console.log("register")
+    }
+
     return (
         <>
             <h1>Signup</h1>
+            <button onClick={handleRegister}>Register</button>
         </>
     )
 }
