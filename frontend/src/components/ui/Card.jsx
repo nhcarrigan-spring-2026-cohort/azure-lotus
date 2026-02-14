0a5323a430035550f5a541757737598ad2797205
manalblack <103968256+manalblack@users.noreply.github.com>
2026-02-14 04:16:03 +0200

    card component updates

--- frontend/src/components/ui/Card.jsx
+++ frontend/src/components/ui/Card.jsx
@@ -1,28 +1,14 @@
 import './Card.css';
 
 
-
-// Instructions
-
-/*
-    Create a reusable card component in components/UI
--Simple styled container DONE
--Card should accept an optional onClick prop to make it clickable when needed. DONE
--Card should accept an optional classname to make additional (custom)styling
--Card should be responsive 
--Card should accept children so that we can put any content inside it. DONE
-
-*/
-
-
-export default function Card({children, className, onClick, ...props}) {
+export default function Card({children, className, onClick, title, ...props}) {
 
     const cardClass = `card ${className}`;
 
     return (
         <div className={`card-container`}>
-            <div className={`${cardClass}`} onClick={onclick} {...props}>
-                <h1>Card component</h1>
+            <div className={`${cardClass}`} onClick={onClick} {...props}>
+                <h2>{title}</h2>
                 {children}
             </div>
         </div>
--- frontend/src/pages/about.jsx
+++ frontend/src/pages/about.jsx
@@ -1,5 +1,4 @@
 import useTitle from '../components/hooks/useTitle';
-import Card from '../components/ui/Card';
 
 export default function about() {
   useTitle('About');
