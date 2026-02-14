15f7b2ff64eb40a422105572b532c44b006c73af
T-Pushyamitra <thamminenipushyamitra@gmail.com>
2026-02-14 11:39:12 +0530

    added missing response model for routers

--- backend/src/features/checkins/routers.py
+++ backend/src/features/checkins/routers.py
@@ -17,13 +17,13 @@ async def senior_daily_checkin(senior_id: int, session: Session = Depends(get_se
     daily_checkin = await get_daily_checkin(senior_id, session)
     return ApiResponse(success=True, message="", data=daily_checkin)
 
-@router.get("/{senior_id}/history", response_model=ApiResponse)
+@router.get("/{senior_id}/history", response_model=ApiResponse[CheckIn])
 async def senior_checkin_history(senior_id: int, session: Session = Depends(get_session)):
     """ Get the history of check-ins of one senior """
     checkin_history = await get_check_in_history(senior_id, session)
     return ApiResponse(success=True, message="", data=checkin_history)
 
-@router.get("/{senior_id}/missing", response_model=ApiResponse)
+@router.get("/{senior_id}/missing", response_model=ApiResponse[CheckIn])
 async def senior_missing_checkin_history(senior_id: int, session: Session = Depends(get_session)):
     """ Get the missing history of check-ins of one senior """
     missing_checkin_history = await get_missing_checkin_history(senior_id, session)
