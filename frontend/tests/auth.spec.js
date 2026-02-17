861d4f03cf7c8e79bf44b9010b3ace09748974b8
Cheryl Murphy <6191116+cherylli@users.noreply.github.com>
2026-02-17 14:32:43 +1100

    add login tests

--- frontend/tests/auth.spec.js
+++ frontend/tests/auth.spec.js
@@ -4,20 +4,29 @@ import {test, expect} from '@playwright/test';
 const baseUrl = 'http://localhost:5173';
 
 test.describe('Signup flow', () => {
-    test('has create family account signup page', async ({page}) => {
-        await page.goto(`${baseUrl}/signup`);
+    test('page exists - create family account signup page', async ({page}) => {
+        const response = await page.goto(`${baseUrl}/signup`);
 
+        await expect(response?.status()).toBe(200)
         await expect(page.getByRole('heading', {name: 'Create Family Account'})).toBeVisible();
+
+    });
+
+    test('page components exist', async ({page}) => {
+        await page.goto(`${baseUrl}/signup`);
 
         await expect(page.getByLabel(/email/i)).toBeVisible();
         await expect(page.getByLabel(/^Password$/i)).toBeVisible();
         await expect(page.getByLabel(/Confirm Password/i)).toBeVisible();
+        await expect(page.getByLabel(/First Name/i)).toBeVisible()
+        await expect(page.getByLabel(/Last Name/i)).toBeVisible()
+        await expect(page.getByLabel(/Phone Number/i)).toBeVisible()
+        await expect(page.locator('#terms-check')).toBeVisible()
+
         await expect(page.getByRole('button', {name: 'Create Account'})).toBeVisible();
+    })
 
-
-    });
-
-    test('signup page - incomplete form should not be submitted', async ({page}) => {
+    test('incomplete form should not be submitted', async ({page}) => {
 
         let requestMade = false
 
@@ -37,7 +46,7 @@ test.describe('Signup flow', () => {
     })
 
 
-    test('signup page submits with mock data', async ({page}) => {
+    test('submits with mock data', async ({page}) => {
         let requestMade = false
 
         await page.route('**/auth/register', async (route) => {
@@ -72,7 +81,74 @@ test.describe('Signup flow', () => {
 })
 
 test.describe('Login flow', () => {
+    test('page exists', async ({page}) => {
+        const response = await page.goto(`${baseUrl}/login`);
+        await expect (response?.status()).toBe(200)
+        await expect(page.getByRole('heading', { name: /welcome back/i})).toBeVisible()
+    })
 
+    test('page components exist', async ({page}) => {
+        await page.goto(`${baseUrl}/login`)
+
+        await expect(page.getByLabel(/username/i)).toBeVisible()
+        await expect(page.getByLabel(/password/i)).toBeVisible()
+        await expect(page.getByRole('button', {name: /log in/i })).toBeVisible()
+    })
+
+    // TODO: Maybe we should prevent empty form being submitted, currently it's allowed
+
+    test('successful login redirects to home page', async ({page}) => {
+        let requestMade = false
+
+        await page.route('**/auth/login', async(route)=> {
+            await route.fulfill({
+                status: 200,
+                contentType: 'application/json',
+                body: JSON.stringify({
+                    "user_info": {
+                        "id": 1,
+                        "email": "user@example.com",
+                        "phone_number": "string",
+                        "first_name": "string",
+                        "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMSIsImVtYWlsIjoidXNlckBleGFtcGxlLmNvbSIsImV4cCI6MTc3MTI5OTM2OCwiaWF0IjoxNzcxMjk3NTY4fQ.OH_DGvWr1aTJg8_DcLi4ZGHhtTR_2PKuGH0igPNBhqk"
+                    }
+                })
+            })
+            requestMade = true
+        })
+
+        await page.goto(`${baseUrl}/login`)
+        await page.getByLabel(/username/i).fill('user@example.com')
+        await page.getByLabel(/password/i).fill('password1234')
+        await page.getByRole('button', {name: /log in/i }).click()
+
+        await expect(page).toHaveURL(`${baseUrl}/`)
+        await expect(requestMade).toBe(true)
+    })
+
+    // TODO: invalid credentials shouldn't be redirected
+    test('unsuccessful login should not be redirected', async ({page}) => {
+        let requestMade = false
+
+        await page.route('**/auth/login', async(route)=> {
+            await route.fulfill({
+                status: 401,
+                contentType: 'application/json',
+                body: JSON.stringify({
+                    "detail": "401: Invalid credentials"
+                })
+            })
+            requestMade = true
+        })
+
+        await page.goto(`${baseUrl}/login`)
+        await page.getByLabel(/username/i).fill('user@example.com')
+        await page.getByLabel(/password/i).fill('password12345')
+        await page.getByRole('button', {name: /log in/i }).click()
+
+        await expect(page).toHaveURL(`${baseUrl}/login`)
+        await expect(requestMade).toBe(true)
+    })
 })
 
 
