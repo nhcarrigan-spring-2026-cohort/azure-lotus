c1216ca5521040d55f99795e6a84b5bf440267f0
Leonardo Cerqueira <leonevescer@gmail.com>
2026-02-28 10:49:12 -0300

    First commit

---
+++ backend/src/features/alerts/routes.py
@@ -0,0 +1,21 @@
+from fastapi import (
+    APIRouter,
+    Depends,
+    Response,
+    HTTPException
+)
+
+from sqlmodel import Session
+from uuid import UUID
+from core.database.session import get_session
+
+from .models import Alert
+
+router=APIRouter()
+
+@router.patch('/alerts/{alert_id}/resolve')
+def resolve_alert(alert_id: UUID, response=Response, db: Session=Depends(get_session)):
+    alert=db.query(Alert).filter(Alert.id == alert_id).first()
+    print(f'Alert obj: {alert}')
+    print(f'Resolved status: {alert.resolved}')
+    return
