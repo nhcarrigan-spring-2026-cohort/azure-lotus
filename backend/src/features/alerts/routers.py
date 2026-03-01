7c4ac02fca3d2f3defaeaeb32655c5b30caaa2fc
Leonardo Cerqueira <leonevescer@gmail.com>
2026-03-01 15:19:34 -0300

    Change resolved field to True

--- backend/src/features/alerts/routers.py
+++ backend/src/features/alerts/routers.py
@@ -10,13 +10,18 @@ from uuid import UUID
 from core.database.session import get_session
 
 from .models import Alert
-
+from shared.api_response import ApiResponse
 
 router=APIRouter()
 
-@router.get('/{alert_id}/resolve')
+@router.get('/{alert_id}/resolve', response_model=ApiResponse)
 async def resolve_alert(alert_id: UUID, response=Response, db: Session=Depends(get_session)):
     alert=db.query(Alert).filter(Alert.id == alert_id).first()
 #    print(f'Alert obj: {alert}')
-    print(f'Resolved status: {alert.resolved}')
-    return alert
+#    print(f'Resolved status: {alert.resolved}')
+
+    alert.resolved=True
+    db.add(alert)
+    db.commit()
+    db.refresh(alert)
+    return ApiResponse(success=True, message="", data=alert)
